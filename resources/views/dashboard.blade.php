a1a8be1469b82bc34914e44bf567fcfb5fbe7460
rhusselroyales13 <rhusselroyales13@gmail.com>
2026-02-26 01:08:22 +0800

    Add CSS and JS links to dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/products.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <title>Albeth's - Bakery</title>
 
 </head>
@@ -39,3 +44,7 @@
 </html>
 
 {{-- <script src="{{ asset('js/script.js') }}"></script> --}}
+<link rel="stylesheet" href="{{ asset('js/script.js') }}">
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="https://kit.fontawesome.com/2952a674d6.js" crossorigin="anonymous"></script>
